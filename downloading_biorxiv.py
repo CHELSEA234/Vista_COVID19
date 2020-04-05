ea819f53be65519c209aff463a51f6cb1d405a85
CHELSEA234 <xiaoguo@usc.edu>
2020-04-04 18:32:37 -0700

    clean code v1

---
+++ downloading_biorxiv.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Created by Xiao Guo
+'''
+Downloading pdf based information in json files.
+The file contains precached failed index of json files in biorxiv_medrxiv.
+'''
+
+import os
+import re
+import sys
+import json
+import glob
+import numpy as np
+import requests			# package to retrive the pdf
+
+from tqdm import tqdm
+
+## step 1: retrieve the json files.
+json_file_dir = '/nas/vista-ssd02/users/xiaoguo/COVID_research/PDFs/biorxiv_medrxiv'
+file_name_list = []
+for file in glob.glob(os.path.join(json_file_dir,"*.json")):
+	file_name_list.append(file)
+
+missing_count = 0
+paper_id_list = []
+biorxiv_paper_sign = '10.1101/'		# the sign for the paper id or DOI
+biorxiv_paper_failed_sign = 'https://doi.org/10.1101/2020.02.'	# does not have paper number
+pre_cached_fail_list = [176, 244, 267, 303, 319, 345, 446, 461, 464, 513, 514, 537, 611, 620, 624, 688, 767]
+fail_index_list = []
+fail_title_list = []
+
+def update_missing_information(file_idx, pdf_dict):
+	'''if current file does not have DOI/hint to the article url.'''
+	cur_split = "dummy"
+	fail_index_list.append(file_idx)
+	fail_title_list.append(pdf_dict['metadata']['title'])
+	global missing_count
+	missing_count += 1
+
+for file_idx, file_name_cur in enumerate(file_name_list):
+	exist_flag = False
+	with open(file_name_cur) as json_file:
+		pdf_dict = json.load(json_file)
+		pdf_str = str(pdf_dict)
+		pdf_str_split = pdf_str.split()
+
+		if file_idx in pre_cached_fail_list:
+			cur_split = "dummy"
+			update_missing_information(file_idx, pdf_dict)
+		else:
+			for cur_split in pdf_str_split:
+				if (biorxiv_paper_sign in cur_split and 
+					cur_split != biorxiv_paper_failed_sign):
+					exist_flag = True
+					if '\'' in cur_split:
+						cur_split = re.sub(r'[^\w\.\:\/]', '', cur_split)
+						if cur_split[-1] == '.':
+							cur_split = cur_split[:-1]
+					break	# get the string (DOI).
+			if not exist_flag:
+				cur_split = "dummy"
+				update_missing_information(file_idx, pdf_dict)
+
+		paper_id_list.append(cur_split)
+
+print(f"{missing_count} files are missing out of {file_idx+1} files in total.")
+
+## dump information for missing files:
+import csv
+write_list = zip(fail_index_list, fail_title_list)
+with open('missing_file.csv', 'a') as f:
+	writer = csv.writer(f, delimiter='\t')
+	writer.writerows(["index", "title"])
+	writer.writerows(write_list)
+
+## step2: to retrieve the pdf.
+## route: doi/hint ==> article url ==> pdf url
+pdf_url_prefix_1 = 'https://www.biorxiv.org/content/'
+pdf_url_prefix_2 = 'https://www.medrxiv.org/content/'
+pdf_url_suffix = 'v1.full.pdf'
+dest_folder = './biorxiv'
+os.makedirs(dest_folder, exist_ok=True)
+
+def retrieve_pdf(url_web, pdf_url_prefix, pdf_url_suffix=pdf_url_suffix):
+	'''
+	to retrieve pdf based on article url.
+	return r and retrival_flag.
+	'''
+	url_full = os.path.join(pdf_url_prefix, url_web+pdf_url_suffix)
+	r = requests.get(url_full, stream=True)
+	retrieval_flag = (r.status_code != 200 and r.status_code != 503)
+	return r, retrieval_flag
+
+for idx, paper_id_cur in tqdm(enumerate(paper_id_list)):
+	if idx in fail_index_list:
+		continue
+	else:
+		if 'https://doi.org/' in paper_id_cur:
+			url_web = paper_id_cur.replace('https://doi.org/', '')
+		elif 'doi.org/' in paper_id_cur:
+			url_web = paper_id_cur.replace('doi.org/', '')
+		elif 'org/' in paper_id_cur:
+			url_web = paper_id_cur.replace('org/', '')
+		else:
+			url_web = paper_id_cur
+
+		r, retrieval_flag = retrieve_pdf(url_web, pdf_url_prefix_1)
+		if not retrieval_flag:
+			r, retrieval_flag = retrieve_pdf(url_web, pdf_url_prefix_2)
+			if not retrieval_flag:
+				print(f"{idx}th paper id, fail to retrive {paper_id_cur}.")
+				continue
+
+		new_pdf_name = str(idx).zfill(5) + '_' + url_web.split('/')[-1]+'.pdf'
+		new_pdf_name = os.path.join(dest_folder, new_pdf_name)
+		with open(new_pdf_name, 'wb') as f:
+			f.write(r.content)
---
+++ paper_title_retrival.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Created by Xiao Guo
+'''
+The script is about downloading pdfs from Commercial use subset, Non-commercial use subset, and Custom license subset.
+Since we had difficulty in downloading pdfs, this scripts save title in each paper for the future usage.
+'''
+
+import os
+import re
+import sys
+import json
+import glob
+import argparse
+import numpy as np
+import requests			# package to retrive the pdf
+import scholarly
+import csv
+
+from pathlib import Path
+
+def csv_file_gen(dataset_name, json_file_dir):
+	'''
+	generate csv file for each dataset.
+	'''
+	json_file_path = os.path.join(json_file_dir, dataset_name)
+	csv_file_name = dataset_name+'_title.csv'
+
+	csv_file = Path(csv_file_name)
+	exist_flag = csv_file.exists()
+	csv_file_handler = open(csv_file_name, mode='a')
+	csv_writer = csv.writer(csv_file_handler, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+	if not exist_flag:	# add title
+		csv_writer.writerow(['ID', 'Json id', 'Title', 'URL'])
+	return json_file_path, csv_file_handler, csv_writer
+
+def json_to_title(json_file_path, debug=False):
+	'''
+	to get title based on each json file.
+	to return list containing all titles.
+	'''
+	json_name_list = []		# json name
+	file_title_list = []	# article title
+	no_title_count = 0
+
+	## take json files from dataset.
+	for idx, json_file in enumerate(glob.glob(os.path.join(json_file_path,"*.json"))):
+		json_name_list.append(json_file)
+		if debug:
+			if idx == 3: break
+
+	## get title from each json.
+	for file_idx, file_name_cur in enumerate(json_name_list):
+		with open(file_name_cur) as json_file:
+			pdf_dict = json.load(json_file)
+			title_cur = pdf_dict['metadata']['title']
+			if title_cur == "":
+				no_title_count += 1
+				title_cur == "dummy"
+			file_title_list.append(title_cur)
+	assert len(file_title_list) == len(json_name_list), print(f"file_title_list \
+		with len {len(file_title_list)} does not match json_name_list with len {len(json_name_list)}.")
+	print(f"{no_title_count} files do not corresponding titles.")
+	return json_name_list, file_title_list
+
+
+def dump_csv(json_name_list, file_title_list, csv_writer):
+	'''
+	to dump information into target csv file.
+	'''
+	for idx, info_list in enumerate(zip(json_name_list, file_title_list)):
+		write_list = [idx]
+		write_list.extend(list(info_list))
+		csv_writer.writerow(write_list)
+
+def retriving_title(dataset_name, json_file_dir, debug_mode):
+	'''
+	downloading pdf given dataset_name.
+	route: dataset path ==> json file ==> title ==> url ==> csv.
+	'''
+	## creating csv file.
+	json_file_path, csv_file_handler, csv_writer = csv_file_gen(dataset_name, json_file_dir)
+
+	## take "title" out of json
+	json_name_list, file_title_list = json_to_title(json_file_path, debug_mode)
+
+	## inject information into csv file.
+	dump_csv(json_name_list, file_title_list, csv_writer)
+
+	print(f"titles in {dataset_name} have been recorded.")
+
+def main(json_file_dir, debug_mode, dataset_name):
+	if dataset_name == "All":
+		dataset_name_list = ["comm_use_subset", "noncomm_use_subset", "pmc_custom_license"]
+	else:
+		dataset_name_list = [dataset_name]
+
+	for dataset_name_cur in dataset_name_list:
+		retriving_title(dataset_name_cur, json_file_dir, debug_mode)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Downloading pdfs from three datasets in COVID_19.')
+    parser.add_argument('-d', '--json_file_dir', default='/nas/vista-ssd02/users/xiaoguo/COVID_research/PDFs/',
+    					help="the directory that contains json files.")
+    parser.add_argument('-de', '--debug_mode', action='store_true', help="debug mode.")
+    parser.set_defaults(debug_mode=False)
+    parser.add_argument('-data', '--dataset_name', default="All", 
+    					choices=['All', "comm_use_subset", "noncomm_use_subset", "pmc_custom_license"],
+    					help="to choose dataset from choices.")
+    args = parser.parse_args()
+    main(json_file_dir=args.json_file_dir, debug_mode=args.debug_mode, dataset_name=args.dataset_name)
